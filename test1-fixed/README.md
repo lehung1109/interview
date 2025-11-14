f8193310b6aec67892d394f916a5114860d11a03
Hung Le <hung.le@optimziely.com>
2025-11-14 09:34:23 +0700

    add test1-fixed

---
+++ test1-fixed/README.md
@@ -0,0 +1,23 @@
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+## Interview
+
+Go to http://localhost:3000/, try to input text to input and observe.
+
+## Requirement
+
+The Change count text change forever, it should be set correctly
+
+**Understand lifecycle can understand why this happen.**
---
+++ test1-fixed/app/SearchBar.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable */
+'use client';
+
+import { useState, useEffect } from "react";
+
+const SearchBar = () => {
+  const [name, setName] = useState("");
+  const [changeCount, setChangeCount] = useState(0);
+
+  useEffect(() => {
+    if(!name) {
+      return;
+    }
+
+    setChangeCount(changeCount + 1);
+  }, [name]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="border rounded px-3 py-2"
+        placeholder="Name"
+      />
+
+      <p className="text-sm mt-2">
+        Change Count: {changeCount}
+      </p>
+    </div>
+  );
+};
+
+export default SearchBar;
