29313139c7ee0766dcc8af472e1fdd7eff08a238
Hung Le <hung.le@optimziely.com>
2026-03-03 09:42:33 +0700

    Add decrease quantity button to Component2: implement functionality to reduce the quantity state, enhancing user interaction.

--- react-js/text-use-prop-in-state/components/component2.tsx
+++ react-js/text-use-prop-in-state/components/component2.tsx
@@ -17,6 +17,7 @@ const Component2 = ({ quantity }: Component2Props) => {
       />
 
       <button className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-600" onClick={() => setValue(value + 1)}>Increase Quantity</button>
+      
       <button className="bg-red-500 text-white px-4 py-2 rounded cursor-pointer hover:bg-red-600" onClick={() => setValue(value - 1)}>Decrease Quantity</button>
     </div>
   );
